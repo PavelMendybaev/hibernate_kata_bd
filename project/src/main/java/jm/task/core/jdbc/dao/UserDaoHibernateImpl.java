ab12e2d925062f70e0a28ee325f89853b5e23d39
Bliss <dansazykin@mail.ru>
2022-01-25 13:38:32 +0500

    Исправленно

--- project/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ project/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 import org.hibernate.*;
 
 import org.hibernate.cfg.Configuration;
@@ -11,24 +12,15 @@ import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
 
-
-    private static final SessionFactory sessionFactory;
-
-    static {
-        try {
-            Configuration configuration = new Configuration().configure()
-                    .addAnnotatedClass(User.class);
-            sessionFactory = configuration.buildSessionFactory();
-        } catch (Throwable ex) {
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
+    private final SessionFactory sessionFactory = Util.getsessionFactory();
 
 
     @Override
     public void createUsersTable(){
+
+
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
 
         String sql = "CREATE TABLE IF NOT EXISTS User  (\n" +
                 "  `id` BIGINT NOT NULL AUTO_INCREMENT,\n" +
@@ -40,64 +32,99 @@ public class UserDaoHibernateImpl implements UserDao {
         session.createSQLQuery(sql).executeUpdate();
 
 
-
-        session.getTransaction().commit();
-        session.close();
-
+        try{
+            transaction.commit();
+        }catch (HibernateException ex){
+            transaction.rollback();
+            ex.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
     }
 
 
     @Override
     public void dropUsersTable() {
+
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
+
 
         String sql = "DROP TABLE IF EXISTS user ;";
-
         session.createSQLQuery(sql).executeUpdate();
 
-        session.getTransaction().commit();
-        session.close();
+
+        try{
+            transaction.commit();
+        }catch (HibernateException ex){
+            transaction.rollback();
+            ex.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
 
         User user = new User(name , lastName , age);
 
         session.save(user);
 
-        session.getTransaction().commit();
-        session.close();
+        try{
+            transaction.commit();
+        }catch (HibernateException ex){
+            transaction.rollback();
+            ex.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
 
         User user = (User) session.load(User.class,id);
         session.delete(user);
 
 
-        session.getTransaction().commit();
-        session.close();
+        try{
+            transaction.commit();
+        }catch (HibernateException ex){
+            transaction.rollback();
+            ex.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
         Session session = sessionFactory.openSession();
-        session.beginTransaction();
+        Transaction transaction = session.beginTransaction();
 
         String sql = "SELECT * FROM user;";
 
         users = session.createSQLQuery (sql).list();
 
-        session.getTransaction().commit();
-        session.close();
+        try{
+            transaction.commit();
+        }catch (HibernateException ex){
+            transaction.rollback();
+            ex.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
 
         return  users;
 
@@ -105,8 +132,24 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
-        dropUsersTable();
-        createUsersTable();
+
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+
+        String sql = "TRUNCATE TABLE user ;";
+        session.createSQLQuery(sql).executeUpdate();
+
+
+        try{
+            transaction.commit();
+        }catch (HibernateException ex){
+            transaction.rollback();
+            ex.printStackTrace();
+        }
+        finally {
+            session.close();
+        }
     }
 
 
--- project/src/main/java/jm/task/core/jdbc/util/Util.java
+++ project/src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private static final SessionFactory sessionFactory ;
+
+    static {
+        try {
+            Configuration configuration = new Configuration().configure()
+                    .addAnnotatedClass(User.class);
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getsessionFactory(){
+
+        return sessionFactory;
+    }
+
 }
